efcdb5f5ca44a1bc0d1c8fc0b37e47927016d61e
tetsuya-k <64536338+tetsuya-ki@users.noreply.github.com>
2020-09-07 18:41:43 +0900

    settings.py忘れたので追加

---
+++ settings.py
@@ -0,0 +1,14 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+DISCORD_TOKEN = os.environ.get("DISCORD_TOKEN")
+IS_DEBUG = os.environ.get("IS_DEBUG")
+REACTION_CHANNELER_100 = int(os.environ.get("REACTION_CHANNELER_100"))
+REACTION_CHANNELER_BELL = int(os.environ.get("REACTION_CHANNELER_BELL"))
+REACTION_CHANNELER_FLAG = int(os.environ.get("REACTION_CHANNELER_FLAG"))
