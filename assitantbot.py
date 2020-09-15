56a38eb29a34245309eec72eebf774377a9b0a5b
tetsuya-k <64536338+tetsuya-ki@users.noreply.github.com>
2020-09-15 23:30:12 +0900

    ヘルプコマンドを修正（変数は太字の斜体とした）

--- assitantbot.py
+++ assitantbot.py
@@ -110,7 +110,7 @@ class Help(commands.HelpCommand):
 
     async def send_command_help(self,command):
         params = ' '.join(command.clean_params.keys())
-        embed = discord.Embed(title=f'{self.context.prefix}{command.qualified_name} {params}',
+        embed = discord.Embed(title=f'{self.context.prefix}{command.qualified_name} ***{params}***',
             description=command.description,color=HELP_COLOR_NORMAL)
         if command.aliases:
             embed.add_field(name='有効なエイリアス：',value='`' + '`, `'.join(command.aliases) + '`',inline=False)
