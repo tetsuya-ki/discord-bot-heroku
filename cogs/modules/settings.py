21414c5eb5ba18d500b8210f0a3d418342538013
tetsuya-k <64536338+tetsuya-ki@users.noreply.github.com>
2020-10-04 21:25:45 +0900

    環境変数読み込み時、Boolean型の読み込みに不具合がある点を修正（True以外でもTrue扱いされてしまっていた）

--- cogs/modules/settings.py
+++ cogs/modules/settings.py
@@ -2,9 +2,16 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+def if_env(str):
+    if str == 'True':
+        return True
+    else:
+        return False
+
 load_dotenv(verbose=True)
 dotenv_path = join(dirname(__file__), 'files' + os.sep + '.env')
 load_dotenv(dotenv_path)
+
 DISCORD_TOKEN = os.environ.get('DISCORD_TOKEN')
-IS_DEBUG = os.environ.get('IS_DEBUG')
+IS_DEBUG = if_env(os.environ.get('IS_DEBUG'))
 AUDIT_LOG_SEND_CHANNEL = int(os.environ.get('AUDIT_LOG_SEND_CHANNEL'))
