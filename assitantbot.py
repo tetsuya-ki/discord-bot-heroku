047c111fae0906178ba4c41d6679e1a58fd2d08f
tetsuya-k <64536338+tetsuya-ki@users.noreply.github.com>
2020-10-30 22:06:14 +0900

    discordpyのバージョンをアップデート(v1.4.1→v1.5.1)し、Intentsの設定を追加し、memberのjoin,removeに対応

--- assitantbot.py
+++ assitantbot.py
@@ -2,7 +2,6 @@ import discord
 from discord.ext import commands # Bot Commands Frameworkをインポート
 from cogs.modules import settings
 import textwrap
-
 import traceback # エラー表示のためインポート
 
 # 読み込むCogの名前を格納
@@ -19,9 +18,9 @@ HELP_COLOR_WARN = 0xaa0000
 # クラス定義。ClientのサブクラスであるBotクラスを継承。
 class AssistantBot(commands.Bot):
     # AssistantBotのコンストラクタ。
-    def __init__(self, command_prefix, help_command):
+    def __init__(self, command_prefix, help_command, intents):
         # スーパークラスのコンストラクタに値を渡して実行。
-        super().__init__(command_prefix, case_insensitive = True, help_command=help_command)
+        super().__init__(command_prefix, case_insensitive = True, help_command=help_command, intents=intents)
         # INITIAL_EXTENSIONに格納されている名前からCogを読み込む。
         # エラーが発生した場合、エラー内容を表示する。
         for cog in INITIAL_EXTENSIONS:
@@ -135,8 +134,12 @@ class Help(commands.HelpCommand):
 
 # AssitantBotのインスタンス化および起動処理。
 if __name__ == '__main__':
+    intents = discord.Intents.default()
+    intents.members = True
+
     bot = AssistantBot(
             command_prefix = '/'
             ,help_command=Help()
+            ,intents=intents
         )# 大文字小文字は気にしない
     bot.run(settings.DISCORD_TOKEN)
