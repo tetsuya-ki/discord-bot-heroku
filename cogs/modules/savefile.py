d7b858ec6ef4b2872e02f95948c783ae89a6f064
tetsuya-k <64536338+tetsuya-ki@users.noreply.github.com>
2020-12-22 08:57:46 +0900

    ダウンロード時のライブラリをurllib.requestからaiohttpに変更。urlの不要部削除

--- cogs/modules/savefile.py
+++ cogs/modules/savefile.py
@@ -1,18 +1,18 @@
 import os
-import urllib.error
-import urllib.request
+import aiohttp
 
 class SaveFile:
     def __init__(self):
         print
 
-    def download_file(self, url, dst_path):
-        try:
-            with urllib.request.urlopen(url) as web_file, open(dst_path, 'wb') as local_file:
-                local_file.write(web_file.read())
-                return dst_path
-        except urllib.error.URLError as e:
-            print(e)
+    async def download_file(self, url, dst_path):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as r:
+                if r.status == 200:
+                    with open(dst_path, 'wb') as local_file:
+                        web_file = await r.read()
+                        local_file.write(web_file)
+                        return dst_path
 
     def download_file_to_dir(self, url, dst_dir):
-        return self.download_file(url, os.path.join(dst_dir, os.path.basename(url)))
+        return self.download_file(url, os.path.join(dst_dir, os.path.basename(url.replace(':large',''))))
